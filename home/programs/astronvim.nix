26317edd4af6c0de728232e2a46a498c82cf0672
Shiruvi <Siruvireu@gmail.com>
2025-11-17 20:25:05 +0300

    adding dependences

--- home/programs/astronvim.nix
+++ home/programs/astronvim.nix
@@ -3,7 +3,12 @@ let
   nvimFHS = pkgs.callPackage ({ buildFHSEnv, writeShellScript, neovim }:
     buildFHSEnv {
       name = "nvim";
-      targetPkgs = pkgs: [ neovim ];
+      targetPkgs = pkgs: [ 
+        neovim
+        python3
+        nodejs
+        cargo
+      ];
       runScript = writeShellScript "nvim.sh" ''
         exec ${neovim}/bin/nvim "$@"
       '';
