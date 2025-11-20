2434bf63b532e079556850f64cbe71586f2c38de
Shiruvi <Siruvireu@gmail.com>
2025-11-20 23:48:20 +0300

    adding neotree

--- home/programs/nvf.nix
+++ home/programs/nvf.nix
@@ -8,6 +8,10 @@
           name = "tokyonight";
           style = "night";
         };
+        filetree.neo-tree = {
+           enable = true;
+           setupOpts.enable_cursor_hijack = true;
+        };
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
