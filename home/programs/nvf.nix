89b3b93ced64370eae7d0456bb193679b4c93efd
Shiruvi <Siruvireu@gmail.com>
2025-11-21 00:25:44 +0300

    adding some lsp servers

--- home/programs/nvf.nix
+++ home/programs/nvf.nix
@@ -3,18 +3,45 @@
     enable = true;
     settings = {
       vim = {
+        globals.mapleader = " ";
         theme = {
           enable = true;
           name = "tokyonight";
           style = "night";
         };
-        filetree.neo-tree = {
-           enable = true;
-           setupOpts.enable_cursor_hijack = true;
+        filetree.nvimTree = {
+            enable = true;
+            mappings = {
+                findFile = "<leader>f";
+                focus = "<leader>o";
+                toggle = "<leader>e";
+                
+            };
         };
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
+        lsp = {
+           enable = true;
+           lspconfig.enable = true;
+           formatOnSave = true;
+        };
+        languages = {
+           enableTreesitter = true;
+           enableFormat = true;
+           bash = {
+                enable = true;
+                format.enable = true;
+                lsp.enable = true;
+                treesitter.enable = true;
+           };
+           nix = {
+                enable = true;
+                format.enable = true;
+                lsp.enable = true;
+                treesitter.enable = true;
+           };
+        };
       };
     };
   };
