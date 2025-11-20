ce39e3ac1cb814d8777c5fb3b9bd35c7decc12a0
Shiruvi <Siruvireu@gmail.com>
2025-11-20 18:40:31 +0300

    nvf config

---
+++ home/programs/nvf.nix
@@ -0,0 +1,13 @@
+{
+  programs.nvf = {
+    enable = true;
+    theme = {
+      enable = true;
+      name = "tokyonight";
+    };
+    settings = {
+      vim.viAlias = true;
+      vim.vimAlias = true;
+    };
+  };
+}
--- nixos/config/Modules.nix
+++ nixos/config/Modules.nix
@@ -3,6 +3,5 @@
     ./users.nix # Users config
     ./services.nix # inline or necessary servises
     ./zsh.nix #shell, please install powerlevel10k zsh theme.
-    ./astronvim.nix
   ];
 }
