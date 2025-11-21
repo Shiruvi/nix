cd4a84be6cdba83e9601b03c660457d07a9325dd
Shiruvi <Shiruvireu@gmail.com>
2025-11-21 09:52:43 +0300

    lsp for nix adding

--- home/programs/nvf.nix
+++ home/programs/nvf.nix
@@ -3,44 +3,33 @@
     enable = true;
     settings = {
       vim = {
-        globals.mapleader = " ";
         theme = {
           enable = true;
           name = "tokyonight";
           style = "night";
         };
         filetree.nvimTree = {
-            enable = true;
-            mappings = {
-                findFile = "<leader>f";
-                focus = "<leader>o";
-                toggle = "<leader>e";
-                
-            };
+          enable = true;
+          mappings = {
+            findFile = "<leader>f";
+            focus = "<leader>o";
+            toggle = "<leader>e";
+          };
         };
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
         lsp = {
-           enable = true;
-           lspconfig.enable = true;
-           formatOnSave = true;
+          enable = true;
+          formatOnSave = true;
+          lspconfig.enable = true;
         };
         languages = {
-           enableTreesitter = true;
-           enableFormat = true;
-           bash = {
-                enable = true;
-                format.enable = true;
-                lsp.enable = true;
-                treesitter.enable = true;
-           };
-           nix = {
-                enable = true;
-                format.enable = true;
-                lsp.enable = true;
-                treesitter.enable = true;
-           };
+          enableTreesitter = true;
+          enableFormat = true;
+          enableLSP = true;
+          #           bash.enable = true;
+          nix.enable = true;
         };
       };
     };
