b07f152c12cab09d60803a6c15d8dcd6b2a3ccd0
Shiruvi <Shiruvireu@gmail.com>
2025-12-05 11:56:12 +0300

    neotree

---
+++ nixos/pkgs/nixvim/neotree.nix
@@ -0,0 +1,12 @@
+{
+plugins.neo-tree.enable = true;
+plugins.neo-tree.settings = {
+close_if_last_window = true;
+  filesystem = {
+    follow_current_file = {
+      enabled = true;
+      leave_dirs_open = true;
+    };
+  };
+};
+}
