6c026f70dec7aa1eac06a59e9e14169f7f3cdc25
Shiruvi <Shiruvireu@gmail.com>
2025-12-05 13:50:19 +0300

    steam derevations

--- nixos/config/game.nix
+++ nixos/config/game.nix
@@ -5,7 +5,12 @@
     enable32Bit = true;
   };
   programs.gamemode.enable = true;
-  programs.steam.enable = true;
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};
   programs.steam.gamescopeSession.enable = true;
   environment.systemPackages = with pkgs; [
     mangohud
